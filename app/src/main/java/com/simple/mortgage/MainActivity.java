a43e118a14165580c96bc539f58f99e75c61a79a
Cansu Hanım <cansu@Cansu-MacBook-Air.local>
2020-10-28 22:48:28 +0300

    index.html sayfasının iskeleti oluşturuldu.

--- app/src/main/java/com/simple/mortgage/MainActivity.java
+++ app/src/main/java/com/simple/mortgage/MainActivity.java
@@ -8,12 +8,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    EditText ToplamKredi,OnOdeme,ToplamVade,YillikFaiz;
-    TextView AylikOdeme;
+    //EditText ToplamKredi,OnOdeme,ToplamVade,YillikFaiz;
+    //TextView AylikOdeme;
+    //Button calculate;
+    //int Hkredi,Honodeme,Htoplamsure,Hfaizorani,HaylikOdeme;
+
+    EditText ToplamKredi;
+    TextView Odeme;
     Button calculate;
-    int Hkredi,Honodeme,Htoplamsure,Hfaizorani,HaylikOdeme;
+
+    int toplampara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +27,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
+        //  ToplamKredi=(EditText) findViewById(R.id.ToplamKredi);
+      //  OnOdeme=(EditText) findViewById(R.id.OnOdeme);
+      //  ToplamVade=(EditText) findViewById(R.id.ToplamVade);
+        //  YillikFaiz=(EditText) findViewById(R.id.YıllıkFaiz);
+        //AylikOdeme=(TextView) findViewById(R.id.AylıkOdeme);
+        //calculate=(Button) findViewById(R.id.calculate);
+
+
+
+
         ToplamKredi=(EditText) findViewById(R.id.ToplamKredi);
-        OnOdeme=(EditText) findViewById(R.id.OnOdeme);
-        ToplamVade=(EditText) findViewById(R.id.ToplamVade);
-        YillikFaiz=(EditText) findViewById(R.id.YıllıkFaiz);
-        AylikOdeme=(TextView) findViewById(R.id.AylıkOdeme);
+        Odeme=(TextView) findViewById(R.id.AylıkOdeme);
         calculate=(Button) findViewById(R.id.calculate);
 
-        calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Hkredi=Integer.parseInt(ToplamKredi.getText().toString());
-                Honodeme=Integer.parseInt(OnOdeme.getText().toString());
-                Htoplamsure=Integer.parseInt(ToplamVade.getText().toString());
-                Hfaizorani=Integer.parseInt(YillikFaiz.getText().toString());
-
-                HaylikOdeme=Hkredi+Honodeme;
-
-                AylikOdeme.setText(Integer.toString(HaylikOdeme));
 
 
-            }
-        });
+calculate.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+        toplampara = Integer.parseInt(ToplamKredi.getText().toString());
+        Odeme.setText(Integer.toString(toplampara));
+
     }
-    }
+});
+
+        //  calculate.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+                    //  public void onClick(View view) {
+                    //Hkredi=Integer.parseInt(ToplamKredi.getText().toString());
+                    //      Honodeme=Integer.parseInt(OnOdeme.getText().toString());
+        //      Htoplamsure=Integer.parseInt(ToplamVade.getText().toString());
+        //Hfaizorani=Integer.parseInt(YillikFaiz.getText().toString());
+
+                    //HaylikOdeme=Hkredi+Honodeme;
+
+        //AylikOdeme.setText(Integer.toString(HaylikOdeme));
+
+
+
+
+        //   }
+        //});
+        //}
+          }
  }
